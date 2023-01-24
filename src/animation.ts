75e43cbb22b23d9ffc26caf3c1f6e64d556f8b0f
Igor Danilov <danilovigor1999@gmail.com>
2023-01-25 00:57:20 +0300

    update:animati

--- src/animation.ts
+++ src/animation.ts
@@ -1,20 +1,27 @@
 export default function anim (){
-    let width:number = +window.innerWidth - 120
+    let width = 0 
+    setInterval(function(){
+    width = +window.innerWidth - 120
+    },1000)
     document.addEventListener('click', function(co:any){
         if (co.target.className == 'start'){
             let m:number = 10
             let i = co.target.id
-            console.log(co.target.className);
-            
             let timer = setInterval(function(){
                 m=m + 20
-                console.log(m);
-                
                 document.querySelector<HTMLInputElement>(`.carColor${i}`).style.left=m+'px'
                 if(m >= width)clearInterval(timer)
+                
             },25) 
+        }
+        if(co.target.className == 'stop') {
+            let s = co.target.id
+            document.querySelector<HTMLInputElement>(`.carColor${s}`).style.left= 45+'px'
+            console.log(s);
+            return
             }else {
                 return
             }  
     })
     }
+    
